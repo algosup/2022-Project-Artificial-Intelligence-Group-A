6eee9b28b18e39b4c6853d0032b7559c6e78712e
David Cuahonte Cuevas <david.cuahonte-cuevas@algosup.com>
2022-05-31 09:22:02 +0200

    get post done

--- Flask try/FlaskTry.py
+++ Flask try/FlaskTry.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
@@ -8,5 +8,18 @@ def home():
     return render_template("index.html", content="Testing")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+	    user = request.form["nm"]
+	    return redirect(url_for("user", usr=user))
+    else:
+	    return render_template("login.html")
+
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+    
 if __name__ == "__main__":
     app.run(debug=True)
