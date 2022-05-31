791afdbf535305ed73b7142801920fb75fe66d07
David Cuahonte Cuevas <david.cuahonte-cuevas@algosup.com>
2022-05-31 12:27:10 +0200

    Flash done

--- Flask try/FlaskTry.py
+++ Flask try/FlaskTry.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, request, session
+from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
 
 app = Flask(__name__)
@@ -17,9 +17,11 @@ def login():
 		session.permanent = True
 		user = request.form["nm"]
 		session["user"] = user
+                flash("Login Successful")
 		return redirect(url_for("user"))
 	else:
 		if "user" in session:
+                    flash("already logged in")
 			return redirect(url_for("user"))
 
 		return render_template("login.html")
@@ -29,12 +31,15 @@ def login():
 def user():
 	if "user" in session:
 		user = session["user"]
-		return f"<h1>{user}</h1>"
+		return render_template("user.html", user=user)
 	else:
 		return redirect(url_for("login"))
 
 @app.route("/logout")
 def logout():
+    if "user" in session:
+	user = session["user"]
+    flash("You were logged out", {user} "info")
     session.pop("user", None)
     return redirect(url_for("login"))
 
