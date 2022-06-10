c51a9bf40db05da9af71d71197f84d27acc32ebb
Laural-Lee Hollande <71769490+lauraleehollande@users.noreply.github.com>
2022-06-10 10:47:57 +0200

    firstPage.html, base.html and style.css ; index.py, jquery.js, test.css, test.js and test.html
    redesign of the web interface, addition of a div to view the percentages and an automatic update of the page
    
    firstPage.html, base.html and style.css was deleted.
    
    index.py -> create random value and function to update
    
    jquery.js -> add jquery
    
    test.css -> page style
    
    test.js -> create a update function and formula for exceptional values
    
    test.html -> create two div for French and English ans structuration

---
+++ AIFlask/Static/test.js
@@ -0,0 +1,38 @@
+var intervalId = window.setInterval(function () {
+    $.ajax({
+        url: '/update',
+        // data: $('form').serialize(),
+        type: 'POST',
+        success: function (response) {
+            response = JSON.parse(response);
+            response = brendon(response);
+            console.log(response);
+
+            document.getElementById("eng").style.width = response[0] + "%";
+            document.getElementById("fr").style.width = response[1] + "%";
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+}, 2000);
+
+function brendon(response) {
+    eng = response[0];
+    fr = response[1];
+    newEng = eng;
+    newFr = fr;
+
+    if (eng + fr > 100) {
+        sum = eng + fr;
+        newEng = eng / sum * 100 - (sum - 100) / 2;
+        newFr = fr / sum * 100 - (sum - 100) / 2;
+
+        console.log(sum, eng, fr);
+
+        if (newEng < 0) { newEng = 0; } 
+        if (newFr < 0) { newFr = 0;}
+    }
+
+    return [newEng, newFr]
+}
--- AIFlask/index.py
+++ AIFlask/index.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template
+import json 
+
+def dummy():
+    return [60, 60]
+
+
+prediction = dummy()
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("firstPage.html")
+    return render_template("test.html")
+
+
+@app.route("/update", methods = ['POST'])
+def getStatus():
+    
+    return json.dumps(prediction)
 
 if __name__ == "__main__":
         app.run(debug = True)
